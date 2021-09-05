6dc47d36b954871b49dd646c44382fb8d4136182
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-05 19:21:21 +0900

    양방향 연관관계와 연관관계의 주인 2 - 주의점, 정리

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -20,6 +20,11 @@ public class Team {
   @OneToMany(mappedBy = "team")
   private List<Member> members = new ArrayList<>();
 
+  public void addMember(Member member) {
+    member.setTeam(this);
+    members.add(member);
+  }
+
   public Long getId() {
     return id;
   }
@@ -42,5 +47,14 @@ public class Team {
 
   public void setMembers(List<Member> members) {
     this.members = members;
+  }
+
+  @Override
+  public String toString() {
+    return "Team{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", members=" + members +
+        '}';
   }
 }
