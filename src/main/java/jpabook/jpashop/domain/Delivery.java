f83ee1c461f051eafeea7cefcba4b6c54a401688
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-06 21:29:59 +0900

    실전 예제 5 - 연관관계 관리

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.domain;
 
+import static javax.persistence.FetchType.LAZY;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -17,6 +19,6 @@ public class Delivery extends BaseEntity {
   private String zipcode;
   private DeliveryStatus status;
 
-  @OneToOne(mappedBy = "delivery")
+  @OneToOne(mappedBy = "delivery", fetch = LAZY)
   private Order order;
 }
