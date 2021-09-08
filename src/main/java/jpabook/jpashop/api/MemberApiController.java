94c0f7c6de574ae1601c26ecda83a272f20c4e33
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-09 00:40:35 +0900

    회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
@@ -12,6 +14,35 @@ import org.springframework.web.bind.annotation.*;
 public class MemberApiController {
 
   private final MemberService memberService;
+
+  @GetMapping("/api/v1/members")
+  public List<Member> memberV1() {
+    return memberService.findMembers();
+  }
+
+  @GetMapping("/api/v2/members")
+  public Result memberV2() {
+    List<Member> findMembers = memberService.findMembers();
+    List<MemberDto> collect = findMembers.stream()
+        .map(m -> new MemberDto(m.getName()))
+        .collect(Collectors.toList());
+
+    return new Result(collect);
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class Result<T> {
+
+    private T data;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class MemberDto {
+
+    private String name;
+  }
 
   @PostMapping("/api/v1/members")
   public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,9 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,9 +17,9 @@ public class Member {
   @Column(name = "member_id")
   public Long id;
 
-  @NotEmpty
   private String name;
 
+  @JsonIgnore
   @Embedded
   private Address address;
 
