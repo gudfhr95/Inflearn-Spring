4b39c6e089cab67c8f4ceeb45f20aed03b6b1f4d
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-09 00:21:06 +0900

    회원 수정 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -4,8 +4,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,6 +26,30 @@ public class MemberApiController {
 
     Long id = memberService.join(member);
     return new CreateMemberResponse(id);
+  }
+
+  @PutMapping("/api/v2/members/{id}")
+  public UpdateMemberResponse updateMemberV2(
+      @PathVariable("id") Long id,
+      @RequestBody @Valid UpdateMemberRequest request
+  ) {
+    memberService.update(id, request.getName());
+    Member findMember = memberService.findOne(id);
+    return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+  }
+
+  @Data
+  static class UpdateMemberRequest {
+
+    private String name;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class UpdateMemberResponse {
+
+    private Long id;
+    private String name;
   }
 
   @Data
