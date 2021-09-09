9338280e63a86b946ec5f46d2d926fa5c0760e8a
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-09 20:30:30 +0900

    OSIV와 성능 최적화

---
+++ src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.service.query;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderQueryService {
+
+  private final OrderRepository orderRepository;
+
+  public List<OrderDto> ordersV3() {
+    List<Order> orders = orderRepository.findAllWithItem();
+
+    List<OrderDto> result = orders.stream()
+        .map(o -> new OrderDto(o))
+        .collect(toList());
+
+    return result;
+  }
+}
