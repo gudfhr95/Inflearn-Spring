ab29ed9fea5661f2612f6d24ca48edd9595bb5f8
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-09 21:52:12 +0900

    QueryDSL 소개

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,20 +1,30 @@
 package jpabook.jpashop.repository;
 
+import static jpabook.jpashop.domain.QMember.member;
+import static jpabook.jpashop.domain.QOrder.order;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
+import jpabook.jpashop.domain.OrderStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
   private final EntityManager em;
+  private final JPAQueryFactory query;
+
+  public OrderRepository(EntityManager em) {
+    this.em = em;
+    this.query = new JPAQueryFactory(em);
+  }
 
   public void save(Order order) {
     em.persist(order);
@@ -90,6 +100,29 @@ public class OrderRepository {
     cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
     TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
     return query.getResultList();
+  }
+
+  public List<Order> findAll(OrderSearch orderSearch) {
+    return query.select(order)
+        .from(order)
+        .join(order.member, member)
+        .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+        .limit(1000)
+        .fetch();
+  }
+
+  private BooleanExpression statusEq(OrderStatus statusCond) {
+    if (statusCond == null) {
+      return null;
+    }
+    return order.status.eq(statusCond);
+  }
+
+  private BooleanExpression nameLike(String nameCond) {
+    if (!StringUtils.hasText(nameCond)) {
+      return null;
+    }
+    return member.name.like(nameCond);
   }
 
   public List<Order> findAllWithMemberDelivery() {
