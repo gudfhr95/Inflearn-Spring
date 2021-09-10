60c016a35b7c559659531b207468c06d26a94adf
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-10 10:56:15 +0900

    파라미터 바인딩

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,8 +3,7 @@ package study.datajpa.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -131,6 +130,19 @@ class MemberRepositoryTest {
     List<MemberDto> memberDto = memberRepository.findMemberDto();
     for (MemberDto dto : memberDto) {
       System.out.println("dto = " + dto);
+    }
+  }
+
+  @Test
+  public void findByNames() {
+    Member m1 = new Member("AAA", 10);
+    Member m2 = new Member("BBB", 20);
+    memberRepository.save(m1);
+    memberRepository.save(m2);
+
+    List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+    for (Member member : result) {
+      System.out.println("member = " + member);
     }
   }
 }
