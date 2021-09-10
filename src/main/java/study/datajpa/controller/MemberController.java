f2f19fbe78703360e91deeaea859913eef83c84a
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-10 19:53:57 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,7 +2,11 @@ package study.datajpa.controller;
 
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -23,8 +27,15 @@ public class MemberController {
     return member.getUsername();
   }
 
+  @GetMapping("/members")
+  public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+    return memberRepository.findAll(pageable).map(MemberDto::new);
+  }
+
   @PostConstruct
   public void init() {
-    memberRepository.save(new Member("userA"));
+    for (int i = 0; i < 100; i++) {
+      memberRepository.save(new Member("user" + i, i));
+    }
   }
 }
