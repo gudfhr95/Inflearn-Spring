1ec1c9b87420475f028fbacbf1ce430679eba9db
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-11 20:08:51 +0900

    동적 쿼리와 성능 최적화 조회 - Builder 사용

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,12 +1,16 @@
 package study.querydsl.repository;
 
+import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
+import study.querydsl.dto.*;
 import study.querydsl.entity.Member;
 
 @Repository
@@ -53,4 +57,31 @@ public class MemberJpaRepository {
         .fetch();
   }
 
+  public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+    BooleanBuilder builder = new BooleanBuilder();
+    if (hasText(condition.getUsername())) {
+      builder.and(member.username.eq(condition.getUsername()));
+    }
+    if (hasText(condition.getTeamName())) {
+      builder.and(team.name.eq(condition.getTeamName()));
+    }
+    if (condition.getAgeGoe() != null) {
+      builder.and(member.age.goe(condition.getAgeGoe()));
+    }
+    if (condition.getAgeLoe() != null) {
+      builder.and(member.age.loe(condition.getAgeLoe()));
+    }
+
+    return queryFactory
+        .select(new QMemberTeamDto(
+            member.id.as("memberId"),
+            member.username,
+            member.age,
+            team.id.as("teamId"),
+            team.name.as("teamName")))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(builder)
+        .fetch();
+  }
 }
