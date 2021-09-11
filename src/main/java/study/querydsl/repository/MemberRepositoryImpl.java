4123e1b82d00ca652c366a5a4f4aa2c46f7999f1
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-11 22:11:58 +0900

    Querydsl 지원 클래스 직접 만들기

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -6,46 +6,25 @@ import static study.querydsl.entity.QTeam.team;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import javax.persistence.EntityManager;
 import org.springframework.data.domain.*;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.*;
 import study.querydsl.entity.Member;
 
-public class MemberRepositoryImpl extends QuerydslRepositorySupport implements
-    MemberRepositoryCustom {
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
   private final JPAQueryFactory queryFactory;
 
   public MemberRepositoryImpl(EntityManager em) {
-    super(Member.class);
     this.queryFactory = new JPAQueryFactory(em);
   }
 
   @Override
   public List<MemberTeamDto> search(MemberSearchCondition condition) {
-
-    List<MemberTeamDto> result = from(member)
-        .leftJoin(member.team, team)
-        .where(
-            usernameEq(condition.getUsername()),
-            teamNameEq(condition.getTeamName()),
-            ageGoe(condition.getAgeGoe()),
-            ageLoe(condition.getAgeLoe())
-        )
-        .select(new QMemberTeamDto(
-            member.id.as("memberId"),
-            member.username,
-            member.age,
-            team.id.as("teamId"),
-            team.name.as("teamName")))
-        .fetch();
-
     return queryFactory
         .select(new QMemberTeamDto(
             member.id.as("memberId"),
@@ -84,30 +63,6 @@ public class MemberRepositoryImpl extends QuerydslRepositorySupport implements
         .offset(pageable.getOffset())
         .limit(pageable.getPageSize())
         .fetchResults();
-
-    List<MemberTeamDto> content = results.getResults();
-    long total = results.getTotal();
-
-    return new PageImpl<>(content, pageable, total);
-  }
-
-  public Page<MemberTeamDto> searchPageSimple2(MemberSearchCondition condition, Pageable pageable) {
-    JPQLQuery<MemberTeamDto> jpaQuery = from(member)
-        .leftJoin(member.team, team)
-        .where(
-            usernameEq(condition.getUsername()),
-            teamNameEq(condition.getTeamName()),
-            ageGoe(condition.getAgeGoe()),
-            ageLoe(condition.getAgeLoe())
-        )
-        .select(new QMemberTeamDto(
-            member.id.as("memberId"),
-            member.username,
-            member.age,
-            team.id.as("teamId"),
-            team.name.as("teamName")));
-
-    JPQLQuery<MemberTeamDto> query = getQuerydsl().applyPagination(pageable, jpaQuery);
 
     List<MemberTeamDto> content = results.getResults();
     long total = results.getTotal();
