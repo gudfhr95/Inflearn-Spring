740fcf61fab39a46e8c810cf4cc30302afeb6a02
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-11 12:02:30 +0900

    예제 도메인 모델과 동작확인

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,26 @@
+package study.querydsl.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+  private String name;
+
+  @OneToMany(mappedBy = "team")
+  private List<Member> members = new ArrayList<>();
+
+  public Team(String name) {
+    this.name = name;
+  }
+}
