85877dd52d254cd043c4264dfe80d6e329cae99f
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-13 20:30:58 +0900

    HttpServletResponse - 기본 사용법

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -1,7 +1,6 @@
-package hello.servlet.request;
+package hello.servlet.basic.request;
 
 import java.io.IOException;
-import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
