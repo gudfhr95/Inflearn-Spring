6a03ef000f4c4a16e1679848859e3df80ff0b8b3
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-09-24 00:13:46 +0900

    홈 화면

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "redirect:/items";
-    }
+  @GetMapping("/")
+  public String home() {
+    return "home";
+  }
 }
