4373bd93124b78d0ec615536bbff27e776193725
Hyungrok <ghldnjsdlf@gmail.com>
2021-10-18 23:16:51 +0900

    시작

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -2,8 +2,9 @@ package hello.exception;
 
 import org.springframework.boot.web.server.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCustomizer implements
     WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
