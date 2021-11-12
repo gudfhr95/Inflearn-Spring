7a936a884398041f6bcbcb59b8556254f83ac6b6
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-11-12 23:45:01 +0900

    전략 패턴 - 예제2

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -89,9 +89,7 @@ public class ContextV1Test {
     context2.execute();
   }
 
-  ₩
   @Test
-
   void strategyV4() {
     ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
     context1.execute();
