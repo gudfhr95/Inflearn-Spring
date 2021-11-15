c2f5aaea7f1b3cce35b526b6195be9b47c76920e
Hyungrok Ham <ghldnjsdlf@gmail.com>
2021-11-15 15:50:39 +0900

    예제 만들기

---
+++ src/main/java/hello/aop/member/annotation/MethodAop.java
@@ -0,0 +1,10 @@
+package hello.aop.member.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodAop {
+
+  String value();
+}
