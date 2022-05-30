bced7362fb4b90d96d24fd8be7a5f185a63c43ea
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-31 00:02:27 +0900

    테스트 - 스프링 부트와 임베디드 모드

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,14 +2,12 @@ package hello.itemservice;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
-import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Slf4j
 //@Import(MemoryConfig.class)
@@ -29,15 +27,15 @@ public class ItemServiceApplication {
     return new TestDataInit(itemRepository);
   }
 
-  @Bean
-  @Profile("test")
-  public DataSource dataSource() {
-    log.info("메모리 데이터베이스 초기화");
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName("org.h2.Driver");
-    dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-    dataSource.setUsername("sa");
-    dataSource.setPassword("");
-    return dataSource;
-  }
+//  @Bean
+//  @Profile("test")
+//  public DataSource dataSource() {
+//    log.info("메모리 데이터베이스 초기화");
+//    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//    dataSource.setDriverClassName("org.h2.Driver");
+//    dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//    dataSource.setUsername("sa");
+//    dataSource.setPassword("");
+//    return dataSource;
+//  }
 }
