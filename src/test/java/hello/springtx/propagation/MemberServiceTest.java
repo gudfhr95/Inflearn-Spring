a8883008657ea437e9b1fbd82bba7ff0d8785d2b
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-06-08 00:02:06 +0900

    트랜잭션 전파 활용2 - 커밋, 롤백

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package hello.springtx.propagation;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,9 +22,9 @@ class MemberServiceTest {
   LogRepository logRepository;
 
   /**
-   * memberService    @Transactional:OFF
+   * MemberService    @Transactional:OFF
    * <p>
-   * memberRepository @Transactional:ON
+   * MemberRepository @Transactional:ON
    * <p>
    * logRepository    @Transactional:ON
    */
@@ -38,5 +39,26 @@ class MemberServiceTest {
     // then: 모든 데이터가 정상 저장된다.
     assertTrue(memberRepository.find(username).isPresent());
     assertTrue(logRepository.find(username).isPresent());
+  }
+
+  /**
+   * MemberService    @Transactional:OFF
+   * <p>
+   * MemberRepository @Transactional:ON
+   * <p>
+   * LogRepository    @Transactional:ON Exception
+   */
+  @Test
+  void outerTxOff_fail() {
+    // given
+    String username = "로그예외_outerTxOff_fail";
+
+    // when
+    assertThatThrownBy(() -> memberService.joinV1(username))
+        .isInstanceOf(RuntimeException.class);
+
+    // then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+    assertTrue(memberRepository.find(username).isPresent());
+    assertTrue(logRepository.find(username).isEmpty());
   }
 }
