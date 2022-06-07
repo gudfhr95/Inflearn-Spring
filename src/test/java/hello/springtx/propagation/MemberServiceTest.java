79592ed255eaef9a8c04f2919f40a06d76cb44ba
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-06-08 00:23:44 +0900

    트랜잭션 전파 활용3 - 단일 트랜잭션

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -61,4 +61,24 @@ class MemberServiceTest {
     assertTrue(memberRepository.find(username).isPresent());
     assertTrue(logRepository.find(username).isEmpty());
   }
+
+  /**
+   * MemberService    @Transactional:ON
+   * <p>
+   * MemberRepository @Transactional:OFF
+   * <p>
+   * LogRepository    @Transactional:OFF
+   */
+  @Test
+  void singleTx() {
+    // given
+    String username = "singleTx";
+
+    // when
+    memberService.joinV1(username);
+
+    // then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+    assertTrue(memberRepository.find(username).isPresent());
+    assertTrue(logRepository.find(username).isPresent());
+  }
 }
